d96b3847c529788a21e6e466dedb83f0e1ba41a5
oceanwater1234 <mrkwunt@gmail.com>
2026-03-13 15:34:46 +0900

    solve 1247

---
+++ pratice/test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+typedef long long ll;
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    //int x=1,y=2,z=3;
+
+    int x,y,z;
+
+    cin >> x >> y >> z;
+
+    
+
+   return 0;
+}
--- pratice/work.cpp
+++ pratice/work.cpp
@@ -1,25 +1,92 @@
 #include<iostream>
-#include<climits>
-#include<string>
 #include<vector>
 #include<algorithm>
 using namespace std;
-typedef long long ll;
-ll abs(ll tmp) {
-    if(tmp >= 0) return tmp;
-    else return -1 * tmp;
-}
-int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+char arr[52][52];
+int main(void) {
 
-	int t = 3;
+	int n, m;
+	vector<int> v;
+	cin >> n >> m;
 
-	while (t--) {
-		vector<ll> v; 
-        ll n,tmp;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			cin >> arr[i][j];
 
-        cin >> n; 
-    }
-			
-   return 0;
+
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (8 <= n - i and 8 <= m - j) {
+				int cnt= 0,min = -100;
+				if (arr[i][j] == 'W') {
+
+					for (int k = i; k < 8 + i; k++) {
+						for (int l = j; l < 8 + j; l++) {
+							if (k % 2 == 0) {
+								if(l % 2 == 0 and arr[k][l] != 'W') cnt++;
+								else if (l % 2 != 0 and arr[k][l] != 'B') cnt++;
+							}
+							else {
+								if (l % 2 == 0 and arr[k][l] != 'B') cnt++;
+								else if (l % 2 != 0 and arr[k][l] != 'W') cnt++;
+							}
+						}
+					}
+					min = cnt , cnt = 0;
+
+					for (int k = i; k < 8 + i; k++) {
+						for (int l = j; l < 8 + j; l++) {
+							if (k % 2 == 0) {
+								if (l % 2 == 0 and arr[k][l] != 'B') cnt++;
+								else if (l % 2 != 0 and arr[k][l] != 'W') cnt++;
+							}
+							else {
+								if (l % 2 == 0 and arr[k][l] != 'W') cnt++;
+								else if (l % 2 != 0 and arr[k][l] != 'B') cnt++;
+							}
+						}
+					}
+
+					min = min > cnt ? cnt : min;
+				}
+
+				else {
+					for (int k = i; k < 8 + i; k++) {
+						for (int l = j; l < 8 + j; l++) {
+							if (k % 2 == 0) {
+								if (l % 2 == 0 and arr[k][l] != 'B') cnt++;
+								else if (l % 2 != 0 and arr[k][l] != 'W') cnt++;
+							}
+							else {
+								if (l % 2 == 0 and arr[k][l] != 'W') cnt++;
+								else if (l % 2 != 0 and arr[k][l] != 'B') cnt++;
+							}
+						}
+					}
+					min = cnt, cnt = 0;
+					for (int k = i; k < 8 + i; k++) {
+						for (int l = j; l < 8 + j; l++) {
+							if (k % 2 == 0) {
+								if (l % 2 == 0 and arr[k][l] != 'W') cnt++;
+								else if (l % 2 != 0 and arr[k][l] != 'B') cnt++;
+							}
+							else {
+								if (l % 2 == 0 and arr[k][l] != 'B') cnt++;
+								else if (l % 2 != 0 and arr[k][l] != 'W') cnt++;
+							}
+						}
+					}
+					min = min > cnt ? cnt : min;
+				}
+
+	
+				v.push_back(min);
+			}
+		}
+	}
+
+	cout << *min_element(v.begin(), v.end());
+
+	return 0;
 }
