4c941be17ce646699793da6824f70ad46bfd187e
oceanwater1234 <mrkwunt@gmail.com>
2026-05-22 04:28:20 +0900

    update sort

--- Algorithm/Sort/bubble.cpp
+++ Algorithm/Sort/bubble.cpp
@@ -1,10 +1,5 @@
 #include<iostream>
-#include<vector>
-#include"student.h"
 using namespace std;
 int main() {
-    vector<Student> v;
-
-    
     return 0;
 }
--- pratice/test.cpp
+++ pratice/test.cpp
@@ -1,6 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main() {
-    
+    char str[] = "123" "12123123"; 
+    cout << str;
     return 0;
 }
